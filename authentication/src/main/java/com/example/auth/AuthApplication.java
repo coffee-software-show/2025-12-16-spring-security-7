af2a9ad6f44af314bdde884681d7c21ed61a7629
Rob Winch <362503+rwinch@users.noreply.github.com>
2025-12-17 12:53:47 -0600

    Simplify

--- authentication/src/main/java/com/example/auth/AuthApplication.java
+++ authentication/src/main/java/com/example/auth/AuthApplication.java
@@ -11,18 +11,23 @@ import org.springframework.security.config.annotation.authorization.EnableMultiF
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.authority.FactorGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.crypto.password4j.Argon2Password4jPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.springframework.security.authorization.AuthenticatedAuthorizationManager.authenticated;
 
 @EnableMultiFactorAuthentication(authorities = {
         FactorGrantedAuthority.PASSWORD_AUTHORITY,
@@ -42,7 +47,7 @@ public class AuthApplication {
         this.port.set(serverInitializedEvent.getWebServer().getPort());
     }
 
-    @Bean
+//    @Bean
     @SuppressWarnings("deprecation")
     DelegatingPasswordEncoder delegatingPasswordEncoder() {
         var encodingId = "password4j-argon2";
@@ -79,9 +84,13 @@ public class AuthApplication {
     }
 
     @Bean
-    JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
-        var users = new JdbcUserDetailsManager(dataSource);
-        users.setEnableUpdatePassword(true);
+    InMemoryUserDetailsManager jdbcUserDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        var users = new InMemoryUserDetailsManager(user);
         return users;
     }
 
